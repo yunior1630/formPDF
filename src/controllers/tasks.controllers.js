c2545e8409b5dc60b3bfc2a635aa04f7d90a56c1
yannyer1620 <94868805+yannyer1620@users.noreply.github.com>
2023-01-24 09:54:37 -0400

    modified:   src/controllers/tasks.controllers.js 	modified:   src/views/layouts/main.hbs 	deleted:    src/views/partials/tasks/taskForm.hbs

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -46,20 +46,3 @@ export const deleteTask = async (req, res, next) => {
   await Task.remove({ _id: id });
   res.redirect("/");
 };
-
-// function genPDF(){
-//   var doc = new jspdf.jsPDF();
-//   var margin=10;
-//   var scale = (doc.internal.pageSize.width - margin * 2)/ document.body.scrollWidth;
-//   doc.html(('./prueba.html'),{
-//     x:margin,
-//     y:margin,
-//     html2canvas:{
-//       scale:scale,
-//     },
-//     callback:function(doc){
-//       doc.output('dataurlnewwindow',{filename:'newPdf.pdf'});
-//     }
-//   })
-
-// }
