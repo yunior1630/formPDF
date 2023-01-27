182f27578317f1d35a0dfcd2462f9c34feffb3aa
yannyer1620 <94868805+yannyer1620@users.noreply.github.com>
2023-01-27 10:49:40 -0400

    configuracion servidor

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -9,18 +9,18 @@ import {
 } from "../controllers/tasks.controllers";
 
 const router = Router();
-
+const routerServer = "/chequemopc"
 // Render all tasks
-router.get("/", renderTasks);
+router.get(routerServer, renderTasks);
 
-router.post("/tasks/add", createTask);
+router.post(routerServer + "/tasks/add", createTask);
 
-router.get("/tasks/:id/toggleDone", taskToggleDone);
+router.get(routerServer + "/tasks/:id/toggleDone", taskToggleDone);
 
-router.get("/tasks/:id/edit", renderTaskEdit);
+router.get(routerServer + "/tasks/:id/edit", renderTaskEdit);
 
-router.post("/tasks/:id/edit", editTask);
+router.post(routerServer + "/tasks/:id/edit", editTask);
 
-router.get("/tasks/:id/delete", deleteTask);
+router.get(routerServer + "/tasks/:id/delete", deleteTask);
 
 export default router;
